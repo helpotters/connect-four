dfa80aeb4dfc90eb76990b16aa6a19cbd3617a56
Paul Lemus <paullemus@protonmail.com>
2021-05-05 15:13:59 -0700

    tdd: print_board added with blank dots conversion

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,13 @@ class Board
   def initialize
     @board_matrix = Array.new(7) { Array.new(6) }
   end
+
+  def print_board
+    blank_dot = "\u26AA".encode('utf-8')
+    board_matrix.each.map do |column|
+      column.each.map do |dot|
+        blank_dot if dot.nil?
+      end
+    end
+  end
 end
--- spec/board_spec_helper.rb
+++ spec/board_spec_helper.rb
@@ -8,6 +8,19 @@ describe Board do
     end
   end
   context 'player sees the board' do
+    describe '#print_board' do
+      context 'the board must be filled with icons' do
+        it 'returns the board with empty slots indicated' do
+          blank_dot = "\u26aa".encode('utf-8')
+          blank_row = Array.new(6, blank_dot)
+          blank_array = Array.new(7, blank_row)
+          expect(board.print_board).to eq(blank_array) # Empty Dot ASCI
+        end
+
+        xit 'returns the board with filled slots indicated' do
+        end
+      end
+    end
   end
 
   context 'player interacts with the board' do
